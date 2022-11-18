567c50e9c704ba9dbae09dc47c841de151564a6f
stackinspector <stackinspector@126.com>
2022-11-18 18:34:28 +0800

    core/std: mod path & adjust naming

--- core/std/mod.rs
+++ core/std/mod.rs
@@ -1,48 +1,10 @@
 #![allow(unused_macros)]
 
-use ::std::collections::BTreeMap;
+use std::collections::BTreeMap;
 use crate::{types::{self, *}, meta};
 
-#[derive(Clone, Debug)]
-pub struct Path {
-    pub path: &'static str,
-    pub name: &'static str,
-    // pub ver: &'static str,
-}
-
-impl Path {
-    pub fn to_path(&self) -> String {
-        macros::concat_string!(
-            "std",
-            ":", self.path,
-            ":", self.name
-        )
-    }
-
-    pub fn to_rust_name(&self) -> String {
-        crate::util::to_pascal_case(self.name)
-    }
-
-    pub fn to_rust_path(&self) -> String {
-        crate::util::to_snake_case(self.path).replace(':', "_")
-    }
-
-    pub fn to_rust_self_path(&self) -> String {
-        macros::concat_string!(
-            "super",
-            "::", self.to_rust_path(),
-            "::", self.to_rust_name()
-        )
-    }
-
-    pub fn to_rust_foreign_path(&self) -> String {
-        macros::concat_string!(
-            "zeon::std::codegen",
-            "::", self.to_rust_path(),
-            "::", self.to_rust_name()
-        )
-    }
-}
+pub mod path;
+use path::StdPath;
 
 pub struct Std {
     pub types: BTreeMap<u16, DefType>,
@@ -53,44 +15,44 @@ pub struct Std {
 
 macro_rules! list {
     ($ty:expr) => {
-        types::Type::List(Box::new($ty))
+        List(Box::new($ty))
     };
 }
 
 macro_rules! map {
     ($tyk:expr, $tyv:expr) => {
-        types::Type::Map(Box::new($tyk), Box::new($tyv))
+        Map(Box::new($tyk), Box::new($tyv))
     };
 }
 
-macro_rules! ty {
+macro_rules! ref_std {
     (:$p:literal :$n:literal) => {{
-        const TY: meta::TypePtr = meta::TypePtr::from_u16_unchecked(const_path2ptr(Path { path: $p, name: $n }));
+        const TY: meta::TypePtr = meta::TypePtr::from_u16_unchecked(const_path2ptr(StdPath { path: $p, name: $n }));
         TY
     }};
 }
 
-macro_rules! alias_t {
+macro_rules! ref_alias {
     (:$p:literal :$n:literal) => {
-        types::Type::Alias(ty!(:$p :$n))
+        Alias(ref_std!(:$p :$n))
     };
 }
 
-macro_rules! c_enum_t {
+macro_rules! ref_c_enum {
     (:$p:literal :$n:literal) => {
-        types::Type::CEnum(ty!(:$p :$n))
+        CEnum(ref_std!(:$p :$n))
     };
 }
 
-macro_rules! enum_t {
+macro_rules! ref_enum {
     (:$p:literal :$n:literal) => {
-        types::Type::Enum(ty!(:$p :$n))
+        Enum(ref_std!(:$p :$n))
     };
 }
 
-macro_rules! struct_t {
+macro_rules! ref_struct {
     (:$p:literal :$n:literal) => {
-        types::Type::Struct(ty!(:$p :$n))
+        Struct(ref_std!(:$p :$n))
     };
 }
 
@@ -137,29 +99,29 @@ macro_rules! def_trait {
     };
 }
 
-macro_rules! def {
+macro_rules! def_std {
     {
         types { $($stdptr:literal | std :$path:literal :$name:literal -> $deftype:expr)* }
         traits { $($stdptr2:literal | std :$path2:literal :$name2:literal -> $deftrait:expr)* }
     } => {
         #[deny(unreachable_patterns)] // deny duplicate ptr
-        pub const fn ptr2path(stdptr: u16) -> Option<Path> {
+        pub const fn ptr2path(stdptr: u16) -> Option<StdPath> {
             Some(match stdptr {
-                $($stdptr => Path { path: $path, name: $name },)*
-                $($stdptr2 => Path { path: $path2, name: $name2 },)*
+                $($stdptr => StdPath { path: $path, name: $name },)*
+                $($stdptr2 => StdPath { path: $path2, name: $name2 },)*
                 _ => return None,
             })
         }
 
-        pub fn path2ptr(path: Path) -> Option<u16> {
+        pub fn path2ptr(path: StdPath) -> Option<u16> {
             Some(match path {
-                $(Path { path: $path, name: $name } => $stdptr,)*
-                $(Path { path: $path2, name: $name2 } => $stdptr2,)*
+                $(StdPath { path: $path, name: $name } => $stdptr,)*
+                $(StdPath { path: $path2, name: $name2 } => $stdptr2,)*
                 _ => return None,
             })
         }
 
-        const fn const_path2ptr(path: Path) -> u16 {
+        const fn const_path2ptr(path: StdPath) -> u16 {
             $(if (
                 crate::util::const_str_equal(path.path, $path) &
                 crate::util::const_str_equal(path.name, $name)
@@ -187,7 +149,7 @@ macro_rules! def {
 
 // endregion
 
-def! {
+def_std! {
     types {
         0x0000 | std :"types" :"deftype" -> def_enum! {
             "alias"  -> Type
@@ -197,7 +159,7 @@ def! {
         }
         0x0001 | std :"prim" :"unix-ts" -> def_alias! (UInt)
         0x0002 | std :"types" :"trait-attr" -> def_struct! {
-            "attr-type" -> enum_t!(:"types" :"trait-attr-type")
+            "attr-type" -> ref_c_enum!(:"types" :"trait-attr-type")
             "attr-name" -> String /* simple-name */
             "val-type"  -> Type
         }
@@ -210,7 +172,7 @@ def! {
         }
         0x0004 | std :"prim" :"simple-name" -> def_alias! (String)
         0x0005 | std :"types" :"trait" -> def_struct! {
-            "attrs"   -> list!(enum_t!(:"types" :"trait-attr"))
+            "attrs"   -> list!(ref_enum!(:"types" :"trait-attr"))
             "extends" -> list!(TypePtr)
         }
         0x0006 | std :"meta" :"rev-type" -> def_c_enum! {
@@ -225,7 +187,7 @@ def! {
             "object"     -> ObjectPtr
             "trait-type" -> TypePtr
             "attr"       -> UInt8
-            "rev-type"   -> c_enum_t!(:"meta" :"rev-type")
+            "rev-type"   -> ref_c_enum!(:"meta" :"rev-type")
         }
         0x0008 | std :"meta" :"commit-ptr" -> def_struct! {
             "ts"  -> Timestamp
@@ -233,9 +195,9 @@ def! {
             "seq" -> UInt16 // reserved for cluster randgen
         }
         0x0009 | std :"meta" :"commit" -> def_struct! {
-            "ptr" -> struct_t!(:"meta" :"commit-ptr")
+            "ptr" -> ref_struct!(:"meta" :"commit-ptr")
             "hash" -> Bytes
-            "revs" -> map!(struct_t!(:"meta" :"rev-ptr"), Unknown) // map unique?
+            "revs" -> map!(ref_struct!(:"meta" :"rev-ptr"), Unknown) // map unique?
         }
     }
     traits {
@@ -243,10 +205,10 @@ def! {
             IterSet "traits" -> TypePtr
         }
         0x8001 | std :"meta" :"name" -> def_trait! {
-            Mut "name" -> alias_t!(:"prim" :"simple-name")
+            Mut "name" -> ref_alias!(:"prim" :"simple-name")
         }
         0x8002 | std :"meta" :"unique-name" -> def_trait! {
-            ;extends ty!(:"meta" :"name"),
+            ;extends ref_std!(:"meta" :"name"),
         }
     }
 }
@@ -266,8 +228,8 @@ mod test {
         assert_eq!(ptr2path(0x0001).unwrap().to_rust_name(), "UnixTs");
         assert_eq!(ptr2path(0x0001).unwrap().to_rust_self_path(), "super::prim::UnixTs");
         assert_eq!(ptr2path(0x0001).unwrap().to_rust_foreign_path(), "zeon::std::codegen::prim::UnixTs");
-        assert_eq!(path2ptr(Path { path: "prim", name: "unix-ts" }).unwrap(), 0x0001);
-        assert_eq!(const_path2ptr(Path { path: "prim", name: "unix-ts" }), 0x0001);
+        assert_eq!(path2ptr(StdPath { path: "prim", name: "unix-ts" }).unwrap(), 0x0001);
+        assert_eq!(const_path2ptr(StdPath { path: "prim", name: "unix-ts" }), 0x0001);
         assert_eq!(std.types.get(&0x0001).unwrap().clone(), DefType::Alias(Type::UInt));
         assert_eq!(format!("{:?}", std.traits.get(&0x8000).unwrap().clone()), r#"Trait { attrs: [TraitAttr { attr_type: IterSet, attr_name: "traits", val_type: TypePtr }], extends: [] }"#);
         assert_eq!(format!("{:?}", std.traits.get(&0x8001).unwrap().clone()), r#"Trait { attrs: [TraitAttr { attr_type: Mut, attr_name: "name", val_type: Alias(Std(StdPtr(4))) }], extends: [] }"#);
---
+++ core/std/path.rs
@@ -0,0 +1,79 @@
+pub fn parts_to_path(s: &str, p: &str, n: &str) -> String {
+    macros::concat_string!(s, ":", p, ":", n)
+}
+
+pub fn to_rust_name(n: &str) -> String {
+    crate::util::to_pascal_case(n)
+}
+
+pub fn to_rust_path(p: &str) -> String {
+    // TODO prevent keywords
+    crate::util::to_snake_case(p).replace(':', "_")
+}
+
+pub fn parts_to_rust_path(root: &str, p: &str, n: &str) -> String {
+    macros::concat_string!(root, "::", to_rust_path(p), "::", to_rust_name(n))
+}
+
+pub fn parts_to_rust_self_path(p: &str, n: &str) -> String {
+    parts_to_rust_path("super", p, n)
+}
+
+#[derive(Clone, Copy, Debug)]
+pub struct StdPath {
+    pub path: &'static str,
+    pub name: &'static str,
+    // pub ver: &'static str,
+}
+
+impl StdPath {
+    pub fn to_path(&self) -> String {
+        parts_to_path("std", self.path, self.name)
+    }
+
+    pub fn to_rust_name(&self) -> String {
+        to_rust_name(self.name)
+    }
+
+    pub fn to_rust_path(&self) -> String {
+        to_rust_path(self.path)
+    }
+
+    pub fn to_rust_self_path(&self) -> String {
+        parts_to_rust_self_path(self.path, self.name)
+    }
+
+    pub fn to_rust_foreign_path(&self) -> String {
+        parts_to_rust_path("zeon::std::codegen", self.path, self.name)
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct Path {
+    pub namespace: String,
+    pub path: String,
+    pub name: String,
+    // pub ver: String,
+}
+
+impl Path {
+    pub fn to_path(&self) -> String {
+        parts_to_path("std", &self.path, &self.name)
+    }
+
+    pub fn to_rust_name(&self) -> String {
+        to_rust_name(&self.name)
+    }
+
+    pub fn to_rust_path(&self) -> String {
+        to_rust_path(&self.name)
+    }
+
+    pub fn to_rust_self_path(&self) -> String {
+        parts_to_rust_self_path(&self.path, &self.name)
+    }
+
+    pub fn to_rust_foreign_path(&self) -> String {
+        parts_to_rust_path("zeon::std::codegen", &self.path, &self.name)
+    }
+}
